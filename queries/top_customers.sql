df8308d95e8894efa9e38a20d1b85ff69bd151c5
bilalasathar <90827668+bilalasathar@users.noreply.github.com>
2025-06-18 23:05:59 +0530

    Delete queries/top_customers.sql

--- queries/top_customers.sql
+++
@@ -1,8 +0,0 @@
--- Top 10 customers by total number of orders
-SELECT 
-    "Customer Name",
-    COUNT(DISTINCT "Order ID") AS total_orders
-FROM superstore
-GROUP BY "Customer Name"
-ORDER BY total_orders DESC
-LIMIT 10;
